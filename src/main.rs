70d035ff490ca948579c555d179c2818dc8949f2
Meph1sto666 <phentragon@gmx.de>
2024-10-21 23:02:28 +0200

    saving before refactor yet again

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,279 @@
-use std::ops::ControlFlow;
-
 use color_eyre::Result;
 use crossterm::event::KeyModifiers;
 use ratatui::{
-    crossterm::event::{ self, KeyCode },
-    layout::{ Constraint, Direction, Layout },
-    widgets::Paragraph,
-    DefaultTerminal, Frame,
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{
+        palette::tailwind::{BLUE, GREEN, SLATE},
+        Color, Modifier, Style, Stylize,
+    },
+    symbols,
+    text::Line,
+    widgets::{
+        Block, Borders, HighlightSpacing, List, ListItem, ListState, Padding, Paragraph,
+        StatefulWidget, Widget, Wrap,
+    },
+    DefaultTerminal,
 };
 use rodio::{OutputStream, Sink};
-// mod components;
 mod playlist;
-// pub use components::playlist::Queue;
+use playlist::{Queue, Song};
+
+const TODO_HEADER_STYLE: Style = Style::new().fg(SLATE.c100).bg(BLUE.c800);
+const NORMAL_ROW_BG: Color = SLATE.c950;
+const ALT_ROW_BG_COLOR: Color = SLATE.c900;
+const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+const TEXT_FG_COLOR: Color = SLATE.c200;
+const COMPLETED_TEXT_FG_COLOR: Color = GREEN.c500;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let terminal: ratatui::Terminal<ratatui::prelude::CrosstermBackend<std::io::Stdout>> = ratatui::init();
-    let app_result: std::result::Result<(), color_eyre::eyre::Error> = run(terminal);
+    let terminal = ratatui::init();
+    let app_result = App::default(
+        &mut Sink::try_new(&OutputStream::try_default().unwrap().1).unwrap()
+        // &mut Sink::new_idle().0
+    ).run(terminal);
     ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    let audio_controls: &mut Sink = &mut Sink::try_new(&OutputStream::try_default().unwrap().1).unwrap();
-    loop {
-        terminal.draw(|frame: &mut Frame<'_>| draw(frame, audio_controls))?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.code == KeyCode::Char('q') && key.modifiers == KeyModifiers::ALT { // no clue why but it only works this way if you want CTRL shortcuts to kill the program
-                audio_controls.stop();
-                return Ok(());
-            }
-            if handle_key_event(key).is_break() {
-                break;
-            }
-        }
-    }
-    Ok(())
+struct App<'a> {
+    // audio_controls: Sink,
+    focus: i8,
+    queue: Queue<'a>,
+    should_exit: bool,
 }
 
-fn draw(frame: &mut Frame, audio_controls: &mut Sink ) {
-    let footer_chunks: std::rc::Rc<[ratatui::prelude::Rect]> = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
+struct TodoList {
+    items: Vec<TodoItem>,
+    state: ListState,
+}
+
+#[derive(Debug)]
+struct TodoItem {
+    todo: String,
+    info: String,
+    status: Status,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Status {
+    Todo,
+    Completed,
+}
+
+impl<'a> App<'a> {
+    fn default(controller: &'a mut Sink) -> Self {
+        Self {
+            should_exit: false,
+            focus: 0,
+            queue: Queue::new::<Song>(controller),
+        }
+    }
+}
+
+impl FromIterator<(Status, &'static str, &'static str)> for TodoList {
+    fn from_iter<I: IntoIterator<Item = (Status, &'static str, &'static str)>>(iter: I) -> Self {
+        let items = iter
+            .into_iter()
+            .map(|(status, todo, info)| TodoItem::new(status, todo, info))
+            .collect();
+        let state = ListState::default();
+        Self { items, state }
+    }
+}
+
+impl TodoItem {
+    fn new(status: Status, todo: &str, info: &str) -> Self {
+        Self {
+            status,
+            todo: todo.to_string(),
+            info: info.to_string(),
+        }
+    }
+}
+
+impl<'a> App<'a> {
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.should_exit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Event::Key(key) = event::read()? {
+                self.handle_key(key);
+            };
+        }
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: KeyEvent) {
+        if key.kind != KeyEventKind::Press {
+            return;
+        }
+        if key.code == KeyCode::Char('q') && key.modifiers == KeyModifiers::ALT {self.should_exit = true}
+        match key.code {
+            KeyCode::Char('a') | KeyCode::Char('A') => self.select_none(),
+            // KeyCode::Char('c') | KeyCode::Char('C') => self.clear(),
+
+            KeyCode::Down => self.select_next(),
+            KeyCode::Up => self.select_previous(),
+            // KeyCode::Char('g') | KeyCode::Home => self.select_first(),
+            // KeyCode::Char('G') | KeyCode::End => self.select_last(),
+            // KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+            //     self.toggle_status();
+            // }
+            _ => {}
+        }
+    }
+
+    fn select_none(&mut self) {
+        self.queue.state.select(None);
+    }
+
+    fn select_next(&mut self) {
+        self.queue.state.select_next();
+    }
+    fn select_previous(&mut self) {
+        self.queue.state.select_previous();
+    }
+
+    fn select_first(&mut self) {
+        self.queue.state.select_first();
+    }
+
+    fn select_last(&mut self) {
+        self.queue.state.select_last();
+    }
+
+    /// Changes the status of the selected list item
+    fn toggle_status(&mut self) {
+        // if let Some(i) = self.queue.state.selected() {
+            // self.queue.songs[i] = match self.queue.songs[i] {
+            //     Status::Completed => Status::Todo,
+            //     Status::Todo => Status::Completed,
+            // }
+        // }
+    }
+}
+
+impl Widget for &mut App<'_> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [tree_area, main_area, queue_area] = Layout::horizontal([
+            // Constraint::from_percentages([20, 50, 30]),
             Constraint::Percentage(20),
             Constraint::Percentage(50),
             Constraint::Percentage(30)
         ])
-        .split(frame.area());
+        .areas(area);
 
-        frame.render_widget(
-        Paragraph::new("Playlist/album etc.")
-        .centered(),
-        footer_chunks[0]
-    );
-    frame.render_widget(Paragraph::new("music player\n- album cover\n- metadata etc."), footer_chunks[1]);
-    frame.render_widget(playlist::Queue::new::<String>(audio_controls), footer_chunks[2]);
+        let [list_area, item_area] = Layout::vertical([Constraint::Fill(1), Constraint::Fill(1)]).areas(main_area);
 
+        // App::render_header(tree_area, buf);
+        // App::render_footer(footer_area, buf);
+        App::render_queue(queue_area, buf);
+        self.render_list(list_area, buf);
+        self.render_selected_item(item_area, buf);
+    }
 }
 
-fn handle_key_event(
-    key: event::KeyEvent
-) -> ControlFlow<()> {
-    if key.code == KeyCode::Char('q') && key.modifiers == KeyModifiers::SHIFT {
-        return ControlFlow::Break(())
+/// Rendering logic for the app
+impl App<'_> {
+    fn render_queue(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Queue")
+            .bold()
+            .centered()
+            .render(area, buf);
     }
-    // match key.code {
 
-    //     _ => (),
-    // }
-    ControlFlow::Continue(())
+    fn render_tree(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Use ↓↑ to move, ← to unselect, → to change status, g/G to go top/bottom.")
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .title(Line::raw("TODO List").centered())
+            .borders(Borders::all());
+            // .border_set(symbols::border::EMPTY)
+            // .border_style(TODO_HEADER_STYLE)
+            // .bg(NORMAL_ROW_BG);
+
+        // Iterate through all elements in the `items` and stylize them.
+        // let items: Vec<playlist::Song> = self
+        //     .queue
+        //     .songs
+        //     .iter()
+        //     .enumerate()
+        //     .map(|(i, s)| {
+        //         let color = alternate_colors(i);
+        //         let song = s;
+        //         playlist::Song::from((*song).clone())
+        //     })
+        //     .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        let mut items: Vec<String> = Vec::new();
+        for item in self.queue.songs {
+            
+            // items.push(item.artist);
+        }
+        let list = List::new(items)
+            .block(block)
+            .highlight_style(SELECTED_STYLE)
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+        // same method name `render`.
+        // StatefulWidget::render(list, area, buf, &mut self.queue.state);
+    }
+
+    fn render_selected_item(&self, area: Rect, buf: &mut Buffer) {
+        // We get the info depending on the item's state.
+        let info = if let Some(i) = self.queue.state.selected() {
+            // match self.queue.songs[i] {
+                // Status::Completed => format!("✓ DONE: {}", self.queue.songs[i].info),
+                // Status::Todo => format!("☐ TODO: {}", self.queue.songs[i].info),
+            // }
+            "Nothing selected...".to_string()
+        } else {
+            "Nothing selected...".to_string()
+        };
+
+        // We show the list item's info under the list in this paragraph
+        let block = Block::new()
+            .title(Line::raw("TODO Info").centered())
+            .borders(Borders::TOP)
+            .border_set(symbols::border::EMPTY)
+            .border_style(TODO_HEADER_STYLE)
+            .bg(NORMAL_ROW_BG)
+            .padding(Padding::horizontal(1));
+
+        // We can now render the item info
+        Paragraph::new(info)
+            .block(block)
+            .fg(TEXT_FG_COLOR)
+            .wrap(Wrap { trim: false })
+            .render(area, buf);
+    }
+}
+
+const fn alternate_colors(i: usize) -> Color {
+    if i % 2 == 0 {
+        NORMAL_ROW_BG
+    } else {
+        ALT_ROW_BG_COLOR
+    }
+}
+
+impl From<&TodoItem> for ListItem<'_> {
+    fn from(value: &TodoItem) -> Self {
+        let line = match value.status {
+            Status::Todo => Line::styled(format!(" ☐ {}", value.todo), TEXT_FG_COLOR),
+            Status::Completed => {
+                Line::styled(format!(" ✓ {}", value.todo), COMPLETED_TEXT_FG_COLOR)
+            }
+        };
+        ListItem::new(line)
+    }
 }
--- src/playlist.rs
+++ src/playlist.rs
@@ -1,66 +1,129 @@
-use ratatui::{buffer::Buffer, layout::Rect, style::Style, widgets::Widget};
+use std::fs::File;
+use ratatui::{ style::{Modifier, Style, Stylize}, text::Text, widgets::{Block, Borders, List, ListItem, ListState}};
 use rodio::Sink;
+use symphonia::core::{formats::FormatOptions, io::{MediaSourceStream, MediaSourceStreamOptions}, meta::{Limit, MetadataOptions}};
+use symphonia::default::get_probe;
 
-pub struct Queue<'a> {
-    controller: &'a mut Sink,
-    current_song: i32,
-    songs: Vec<String>
+pub struct Song {
+    path: String,
+	title: String,
+	artist: String,
+    album: String,
+    track_num: String,
+    album_tracks_total: String,
+    year: String,
+    source: File,
+    stream: MediaSourceStream
 }
 
-impl<'a> Queue<'a> {
-    pub fn new<T>(controller: &'a mut Sink) -> Self {
-        Queue {
-            controller: controller,
-            current_song: -1,
-            songs: Vec::new()
-        }
-    }
+impl Song {
+    pub fn new(path: String) -> Self {
+        let file: File = File::open(&path).expect("Failed to open file");
+        let probe = get_probe();
+        let mut format = probe.format(
+            &Default::default(),
+            MediaSourceStream::new(
+                Box::new(file),
+                MediaSourceStreamOptions { buffer_len: (1024*512) }
+            ),
+            &FormatOptions { prebuild_seek_index: true, seek_index_fill_rate: 128, enable_gapless: true },
+            &MetadataOptions {
+                limit_metadata_bytes: Limit::None,
+                limit_visual_bytes: Limit::None
+            }
+        ).expect("Failed to probe format");
+        
 
-    pub fn add_song<T: Into<String>>(&mut self, song: T) {
-        self.songs.push(song.into());
-    }
-}
-impl<'a> Widget for Queue<'a> {
-    // #[allow(clippy::cast_possible_truncation)]
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        // let (background, text, shadow, highlight) = self.colors();
-        buf.set_style(area, Style::new());
+        let dta = format.metadata.get().unwrap();        
+        let tags = dta.current().unwrap().tags();
+        // println!();
+        Song {
+            path: path.clone(),
+            title: tags[0].value.to_string(),
+            artist: tags[1].value.to_string(),
+            album: tags[4].value.to_string(),
+            track_num: tags[2].value.to_string(),
+            album_tracks_total: tags[3].value.to_string(),
+            year: (tags[5].value.to_string()),
+            // "TITLE:{}, ARTIST:{}, ALBUM:{}, TRACK_NUM:{}, TRACK_TOTAL:{}, YEAR:{}", tags[0].value, tags[1].value, tags[4].value, tags[2].value, tags[3].value, tags[5].value
 
-        // render top line if there's enough space
-        if area.height > 2 {
-            buf.set_string(
-                area.x,
-                area.y,
-                "▔".repeat(area.width as usize),
-                Style::new()
-            );
+            source: std::fs::File::open(&path).expect("failed to open media"),
+            stream: MediaSourceStream::new(Box::new(std::fs::File::open(&path).expect("failed to open media")), Default::default()),
+            // source: Decoder::new(BufReader::new(File::open(path).unwrap())).unwrap()
         }
-        // render bottom line if there's enough space
-        if area.height > 1 {
-            buf.set_string(
-                area.x,
-                area.y + area.height - 1,
-                "▁".repeat(area.width as usize),
-                Style::new()
-            );
-        }
-        // render label centered
-        // buf.set_line(
-        //     area.x + (area.width.saturating_sub(self.label.width() as u16)) / 2,
-        //     area.y + (area.height.saturating_sub(1)) / 2,
-        //     &self.label,
-        //     area.width,
-        // );
-    }
+	}
 }
 
-// impl Queue<'_> {
-//     const fn colors(&self) -> (Color, Color, Color, Color) {
-//         let theme = self.theme;
-//         match self.state {
-//             State::Normal => (theme.background, theme.text, theme.shadow, theme.highlight),
-//             State::Selected => (theme.highlight, theme.text, theme.shadow, theme.highlight),
-//             State::Active => (theme.background, theme.text, theme.highlight, theme.shadow),
+// impl Clone for Song {
+//     fn clone(&self) -> Self {
+//         return Song {
+//             path: self.path.clone(),
+//             title:self.title.clone(),
+//             artist:self.artist.clone(),
+//             album:self.album.clone(),
+//             track_num:self.track_num.clone(),
+//             album_tracks_total:self.album_tracks_total.clone(),
+//             year: self.year.clone(),
+//             source: self.source,
+//             stream: self.stream
 //         }
 //     }
 // }
+
+// impl Copy for Song {
+//     fn Copy(value: Song) -> Self {
+//         return Song {value: value.}
+//     }
+// }
+
+impl From<Song> for Text<'_> {
+    fn from(value: Song) -> Self {
+        return Text::raw(value.title)
+    }
+}
+pub struct  Queue<'b>  {
+    pub ui_list: List<'b>,
+    pub state: ListState,
+    pub songs: Vec<Song>,
+    playing_song: i32
+}
+
+impl<'b> Queue<'b> {
+    // pub fn new<T>(controller: &'b mut Sink, songs: Vec<Song>) -> Self {
+    pub fn new<T>(controller: &'b mut Sink) -> Self {
+        let items: Vec<ListItem> = Vec::new();
+        // for s in songs { // create list of songs displayed in the queue tab
+        //     let mut txt = ratatui::text::Text::raw("");
+
+        //     txt.push_span(format!("{}\n", s.title.clone()).bold());
+        //     txt.push_span(" - ");
+        //     txt.push_span(format!("{}", s.artist.clone()).gray());
+
+        //     items.push( ListItem::new( txt ) );
+        // }
+
+        let block =  Block::new()
+            .border_type(ratatui::widgets::BorderType::Rounded)
+            .title("[Q]ueue")
+            .borders(Borders::all())
+            .border_style(Style::new().red()); // define the border around the queue
+        Queue {
+            // controller: &mut Sink::try_new(&OutputStream::try_default().unwrap().1).unwrap(),
+            ui_list: List::new(items).block(block),
+            controller: controller,
+            playing_song: -1,
+            songs: Vec::new(),
+            state: ListState::default()
+        }
+    }
+
+    pub fn hide(mut self) {
+        self.ui_list = self.ui_list.add_modifier(Modifier::HIDDEN);
+    }
+    pub fn unhide(mut self) {
+        self.ui_list = self.ui_list.remove_modifier(Modifier::HIDDEN);
+    }
+    pub fn down(mut self) {
+        self.state.select_next();
+    }
+}
